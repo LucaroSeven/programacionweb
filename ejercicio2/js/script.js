d14de9e108269eadfc10164cffc3677a52dc93d7
Luis Carrion <luiscarrionrdz@gmail.com>
2020-10-18 18:42:57 -0700

    Ejercicio 2 terminado

--- ejercicio2/js/script.js
+++ ejercicio2/js/script.js
@@ -1,10 +1,43 @@
 function doSum(){
-    var num1 = document.getElementById("inputNum1");
-    var num2 = document.getElementById("inputNum2");
+    var num1 = document.getElementById("inputSumNum1");
+    var num2 = document.getElementById("inputSumNum2");
 
     var result = Number(num1.value) + Number(num2.value);
     //console.log(result);
 
     var lblResult = document.getElementById("lblSumResult");
+    lblResult.innerHTML = "Result: "+ result;
+}
+
+function doRes(){
+    var num1 = document.getElementById("inputResNum1");
+    var num2 = document.getElementById("inputResNum2");
+
+    var result = Number(num1.value) - Number(num2.value);
+    //console.log(result);
+
+    var lblResult = document.getElementById("lblResResult");
+    lblResult.innerHTML = "Result: "+ result;
+}
+
+function doMult(){
+    var num1 = document.getElementById("inputMultNum1");
+    var num2 = document.getElementById("inputMultNum2");
+
+    var result = Number(num1.value) * Number(num2.value);
+    //console.log(result);
+
+    var lblResult = document.getElementById("lblMultResult");
+    lblResult.innerHTML = "Result: "+ result;
+}
+
+function doDiv(){
+    var num1 = document.getElementById("inputDivNum1");
+    var num2 = document.getElementById("inputDivNum2");
+
+    var result = Number(num1.value) / Number(num2.value);
+    //console.log(result);
+
+    var lblResult = document.getElementById("lblDivResult");
     lblResult.innerHTML = "Result: "+ result;
 }
--- ejercicio2/js/scripts2.js
+++ ejercicio2/js/scripts2.js
@@ -1,9 +1,62 @@
-function saveArticle(){
-    var article = document.getElementById("inputArticle");
+// function saveArticle(){
+//     var article = document.getElementById("inputArticle");
 
-    var ulArticles = document.getElementById("ulArticles");
-    ulArticles.innerHTML += "<li>"+article.value+"</li>"
+//     var ulArticles = document.getElementById("ulArticles");
+//     ulArticles.innerHTML += "<li>"+article.value+"</li>"
 
-    article.value = ""
-    article.focus();
+//     article.value = ""
+//     article.focus();
+// }
+
+function add(){
+    var make = document.getElementById("inputMake");
+    var model = document.getElementById("inputModel");
+    var year = document.getElementById("inputYear");
+
+    var table = document.getElementById("table")
+
+    if (make.value == "" || model.value == "" || year.value == "") {
+        var camposVacios = "";
+        camposVacios += make.value == "" ? "Make " : "";
+        camposVacios += model.value == "" ? "Model " : "";
+        camposVacios += year.value == "" ? "Year " : "";
+        alert("Campos vacios: "+camposVacios);
+
+        return;
+    }
+
+    table.innerHTML += 
+    `
+    <tr id="tableRow">
+        <td>${make.value}</td>
+        <td>${model.value}</td>
+        <td>${year.value}</td>
+    </tr>
+    `;
+    // alert("Datos insertados");
+}
+
+function clearInput(){
+    var row = document.getElementById("tableRow");
+    var table = document.getElementById("table")
+
+    if (row == null) {
+        alert("No se encontro fila");
+        return;
+    }
+    table.deleteRow(1);
+}
+
+function deleteAll(){
+    var table = document.getElementById("table");
+    var rows = table.rows.length;
+
+    if (rows == 1) {
+        alert("No hay datos")
+    }
+
+    console.log("Rows: "+rows)
+    for (let i = 1; i < rows; i++) {
+        table.deleteRow(1);
+    }
 }
